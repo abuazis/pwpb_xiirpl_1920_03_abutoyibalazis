a36562e9de4477575677a865e590910f19b2c29d
Abu Toyib Al Azis <muhammadazis801@gmail.com>
2019-08-24 15:56:38 +0700

    menambahkan grafik untuk fitur laporan

---
+++ front-end/assets/custom/chart.js
@@ -0,0 +1,103 @@
+var getChart1 = document.getElementById('laporanOrder');
+var myChart1 = new Chart(getChart1, {
+    type: 'bar',
+    data: {
+        labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+// ==============================================================================
+
+var getChart2 = document.getElementById('laporanMenu');
+var myChart2 = new Chart(getChart2, {
+    type: 'pie',
+    data: {
+        labels: [
+            "Pizza",
+            "Pasta",
+            "Chicken",
+            "Dessert",
+            "Drink"
+        ],
+        datasets: [{
+            data: [133.3, 86.2, 52.2, 51.2, 50.2],
+            backgroundColor: [
+                "#FF6384",
+                "#63FF84",
+                "#84FF63",
+                "#8463FF",
+                "#6384FF"
+            ]
+        }]
+    }
+});
+
+// ===============================================================================
+
+var getChart3 = document.getElementById('laporanHarian');
+var dataFirst = {
+    label: "Car A - Speed (mph)",
+    data: [0, 59, 75, 20, 20, 55, 40],
+    lineTension: 0,
+    fill: false,
+    borderColor: 'red'
+  };
+
+var dataSecond = {
+    label: "Car B - Speed (mph)",
+    data: [20, 15, 60, 60, 65, 30, 70],
+    lineTension: 0,
+    fill: false,
+  borderColor: 'blue'
+  };
+
+var speedData = {
+  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+  datasets: [dataFirst, dataSecond]
+};
+
+var chartOptions = {
+  legend: {
+    display: true,
+    position: 'top',
+    labels: {
+      boxWidth: 80,
+      fontColor: 'black'
+    }
+  }
+};
+var myChart3 = new Chart(getChart3, {
+    type: 'line',
+    data: speedData,
+    options: chartOptions
+});
